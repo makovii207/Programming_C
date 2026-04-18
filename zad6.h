89b93f6f9b3a185bec901d7d71788f6fa36cd903
makovii207 <charon.baby2000@mail.ru>
2026-04-18 14:21:01 +1000

    7 лаба поправил

--- zad6.h
+++ zad6.h
@@ -2,23 +2,23 @@
 #include <stdio.h>
 
 static int zad6() {
-    FILE *f_in = fopen("people.txt", "r");
-    FILE *f_out = fopen("filtered_people.txt", "w");
+    FILE *fin = fopen("people.txt", "r");
+    FILE *fout = fopen("filtered_people.txt", "w");
 
     char f[50], i[50], o[50];
     int year;
 
     if (f_in == NULL) return 1;
 
-    while (fscanf(f_in, "%s %s %s %d", f, i, o, &year) != EOF) {
+    while (fscanf(fin, "%s %s %s %d", f, i, o, &year) != EOF) {
         if (year > 1980) {
-            fprintf(f_out, "%s %s %s %d\n", f, i, o, year);
+            fprintf(fout, "%s %s %s %d\n", f, i, o, year);
         }
     }
 
     // Закрываем файлы
-    fclose(f_in);
-    fclose(f_out);
+    fclose(fin);
+    fclose(fout);
 
     return 0;
 }
